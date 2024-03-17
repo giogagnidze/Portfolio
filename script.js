6ef51201ef3a4ba914710befa5fccdb90e30254a
giogagnidze <giogagnie@gmail.com>
2024-03-17 20:40:29 +0400

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let typed = new Typed(".auto-type", {
+    strings: ["TheCodeCrafter", "Giorgi Gagnidze", "Web Developer", ""],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+})
+
+function reveal() {
+let reveals = document.querySelectorAll(".reveal");
+
+for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+    } else {
+        reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
